3d59848b5c06c25d90a83d7dceeef55d1c689605
fesepc <fe.sepulveda.c@gmail.com>
2017-04-19 22:30:06 -0300

    alignment test to select att

--- extract_sccmec.py
+++ extract_sccmec.py
@@ -3,6 +3,9 @@ import sys
 import regex as re
 import subprocess
 
+
+
+
 def config():
 	import ConfigParser
 	config = ConfigParser.ConfigParser()
@@ -28,6 +31,7 @@ def simple_sequence(file):
 
     return sequence
 
+
 def blast(blast_exe, database, query):
 	outfmt = "6 qseqid qlen sseqid slen qstart qend sstart send length nident pident evalue"
 	cmd = [blast_exe, "-outfmt", outfmt, "-db", database]
@@ -39,12 +43,14 @@ def blast(blast_exe, database, query):
 
 	return results, err
 
+
 def makeblastdb(makeblastdb_exe, working_dir, input_seq, nucl, db_name):
 	os.chdir(working_dir)
 	proc = makeblastdb_exe+' -in '+input_seq+' -dbtype '+nucl+' -out '+db_name
 	os.system(proc)
 
 	return db_name
+
 
 def prokka_files(location):
 	for file in os.listdir(location):
@@ -59,6 +65,7 @@ def prokka_files(location):
 
 	return ffn, gff, fna, faa
 
+
 def execute_prokka(prokka_exe, output_prokka, contigs):
 	kingdom = "Bacteria"
 	genus = "Staphylococcus"
@@ -71,6 +78,7 @@ def execute_prokka(prokka_exe, output_prokka, contigs):
 					" --centre 10 --compliant"+ \
 					' '+contigs
 	os.system(cmd_prokka)
+
 
 def fasta2dict(file):
 	fastadict = {}
@@ -89,12 +97,14 @@ def fasta2dict(file):
 
 	return fastadict
 
+
 def get_sequence(sequences_dict, sequence_id):
 	sequence = ""
 	for i in sequences_dict.get(sequence_id)[:]:
 		sequence += ''.join(i)
 
 	return sequence
+
 
 #import string
 #import random
@@ -111,11 +121,13 @@ def reverse_complement(seq):
 	reverse = complement[::-1]
 	return reverse
 
+
 def blast_parser(blast_exe, db, query):
 	blastfile, err = blast(blast_exe, db, query)
 	best_hit = blastfile.split()[2]
 
 	return best_hit
+
 
 def get_contig(gff, query):
 	with open(gff) as f:
@@ -127,8 +139,10 @@ def get_contig(gff, query):
 
 	return header[0]
 
+
 def checkContig(lst):
 	return lst[1:] == lst[:-1]
+
 
 def checkSense(sequence, contig):
 	if re.findall("{pattern}".format(pattern=sequence), contig):
@@ -137,41 +151,13 @@ def checkSense(sequence, contig):
 	if re.findall("{pattern}".format(pattern=sequence), contig):
 		return contig, "+"
 
-def clean_att(atts_location):
-	"""
-	Conditions to verify if seq is att,
-	"""
-	core = "TATCA"
-	adjacent_core = "GA.G"
-	stop_codons = ["TAA", "TGA"]
-
-	#for k in atts_location.keys():
-		#print k
-	#	if not k[-3:] in stop_codons:
-	#		del atts_location[k]
-	for k in atts_location.keys():
-		if not core in k:
-			del atts_location[k]
-	for k in atts_location.keys():
-		if not re.findall("{pattern}".format(pattern=adjacent_core), k):
-			del atts_location[k]
-
-	cassette_coordinates = []
-	for k, v in atts_location.iteritems():
-		print "att: ", k, "location: ", v
-		cassette_coordinates.append(v[0])
-
-	sites = sorted(atts_location.items(), key=lambda e: e[1][0])
-	from itertools import chain
-	coordinates = sorted(list(chain(*cassette_coordinates)))
-
-	return coordinates, sites, atts_location
 
 def create_dir(base, dir_name):
 	new_dir = os.path.join(base, dir_name)
 	if not os.path.isdir(new_dir):
 		os.makedirs(new_dir)
 	return new_dir
+
 
 def find_position(sequence, gene, name):
 	if re.findall("{pattern}".format(pattern=gene), sequence):
@@ -180,6 +166,62 @@ def find_position(sequence, gene, name):
 	else:
 		for i in re.findall("{pattern}".format(pattern=reverse_complement(gene)), sequence):
 			print "- %s located at: " % name, [(m.start(0), m.end(0)) for m in re.finditer("{}".format(i), sequence)]
+
+
+def findAtts(nombre, output, sequence, att):
+	os.chdir(output)
+	"""?att patron basico corresponde a los nucl finales del gen orfX"""
+	atts_location = {}
+	for i in re.findall("({s}){{s<=5}}".format(s=att), sequence):
+		atts_location[sequence[m.start(0):(m.end(0))]] = [(m.start(0), m.end(0)) for m in re.finditer("{}".format(i), sequence)]
+
+	core = "TATCATAA"
+	adjacent_core = "GA[ACTG]G"
+	""" eliminar att encontrados que no posean la secuencia central de att"""
+	for key in atts_location.keys():
+		if not core in key:
+			del atts_location[key]
+	for k in atts_location.keys():
+		if not re.findall("{s}".format(s=adjacent_core), k):
+			del atts_location[k]
+
+	""" ordenar elementos en att_location segun posicion (creciente)"""
+	sites = sorted(atts_location.items(), key=lambda e: e[1][0])
+	attL_pos = int([x+y for (x,y) in sites[0][1]][0])
+
+	hipotetical_attL = {}
+	hipotetical_attR = {}
+
+	val_attl = int([x+y for (x,y) in sites[0][1]][0])
+
+	for s in sites:
+		fr = s[1][0][0]
+		to = s[1][0][1]
+		if fr+to > (val_attl+300):
+			hipotetical_attR[s[0]] = s[1][0]
+		else:
+			hipotetical_attL[s[0]] = s[1][0]
+
+		attL_list = sorted(hipotetical_attL.items(), key=lambda e: e[1][0])
+		attR_list = sorted(hipotetical_attR.items(), key=lambda e: e[1][0])
+
+		attL_sequence = sequence[attL_list[0][1][0]-20:attL_list[0][1][1]+20]
+		with open("attL_"+nombre+".fasta", "w") as f:
+			f.write(">attL_"+nombre+"\n")
+			f.write(attL_sequence+'\n')
+
+		with open("attR_"+nombre+".fasta", "w") as f:
+			for attr in attR_list:
+				#print attr
+				f.write(">attR_"+nombre+"_"+str(attr[1][0]-20)+"_"+str(attr[1][1]+20)+"\n")
+				f.write(str(sequence[attr[1][0]-20:attr[1][1]+20])+'\n')
+				#print seq[attr[1][0]-20:attr[1][1]+20]
+
+	print attL_list[0][1][0], "caca"
+	print attR_list[-1][1][1], "xxx"
+	coordinates = [attL_list[0][1][0], attR_list[-1][1][1]]
+
+	return coordinates
 
 
 # ------------------------------------------------------------------------- #
@@ -265,57 +307,17 @@ def main():
 		print att_actual_orfx
 
 		""" Search for attachment site sequences """
-		atts_location = {}	
-		for i in re.findall("({att}){{s<=5}}".format(att=att_actual_orfx), seq):
-			#print [(m.start(0), m.end(0)) for m in re.finditer("{}".format(i), seq)], i
-			atts_location[seq[m.start(0):(m.end(0))]] = [(m.start(0), m.end(0)) for m in re.finditer("{}".format(i), seq)]
-
 		""" Filter att sequences according to literature """
-		coordinates, sites, cleaned_atts_location = clean_att(atts_location)
+		coordinates = findAtts(nombre, raw_data, seq, att_actual_orfx)
 		sccmec = seq[coordinates[0]:coordinates[-1]]
-
-
-		#print sites[0][1][0]
-		attL = sites[0][1][0]
-		#print attL[0], attL[1]
-		#print seq[attL[0]-30:attL[1]+30]
-		attL_seq = seq[attL[0]-30:attL[1]+30]
-
-		#print sites[-1][1][0]
-		attR = sites[-1][1][0]
-		#print attR[0], attR[1]
-		#print seq[attR[0]-30:attR[1]+30]
-		attR_seq = seq[attR[0]-30:attR[1]+30]
-
-
-		print
-		print("-"*78)
-		print
 
 		print "Contig Length: ", len(seq)
 		print "SCCmec Length: ", len(sccmec)
-
-		os.chdir(raw_data)
-
 		""" EDITAR """
-		with open("attL_"+nombre+".fasta", "w") as f:
-			f.write(">attL_"+nombre+"\n")
-			f.write(attL_seq+'\n')
-		with open("attR_"+nombre+".fasta", "w") as f:
-			f.write(">attL_"+nombre+"\n")
-			f.write(attR_seq+'\n')
 		with open("sccmec_"+nombre+".fasta", "w") as f:
 			f.write(">sccmec_"+nombre+"_"+str(len(sccmec))+"\n")
 			for i in range(0, len(sccmec), 60):
 				f.write(sccmec[i:i+60]+'\n')
-		with open("info.txt", "w") as f:
-			f.write("General Information"+ '\n'+'\n')
-			f.write("Strand Sense: "+orfx_sense+'\n')
-			f.write("Contig Length: "+str(len(seq))+'\n')
-			f.write("SCCmec Length: "+str(len(sccmec))+'\n'+'\n')
-			f.write("Attachment Site Sequences"+'\n'+'\n')
-			for k, v in cleaned_atts_location.iteritems():
-				f.write("att: " + str(k) + " location: " + str(v[0]) + '\n')
 
 	else:
 		print "Error: core components are not in the same contig"
