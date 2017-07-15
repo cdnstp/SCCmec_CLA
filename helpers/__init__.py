15f32845a1ff5277ba63bd93df3b97d29dd46b4b
fesepc <fe.sepulveda.c@gmail.com>
2017-07-14 20:48:24 -0400

    adding graphical representation of sccmec

--- helpers/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-from basic_helpers import *
-from blast_helpers import *
-from prokka_helpers import *
-from attachment import *
-from current_classification import *
-from mash_network import *
